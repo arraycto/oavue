b1c18f12b9ec7bcbc1181857088bfe75e6bf881f
songjing-Git <1249932920@qq.com>
2020-12-10 16:05:21 +0800

    home logo router

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,23 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
+import Home from "../views/Home";
+import Login from "../views/Login";
 Vue.use(Router)
 
 export default new Router({
+    mode:"history",
     routes: [
         {
-
+            path: '',
+            redirect: 'login'
+        },
+        {
+            path: '/login',
+            component: Login
+        },
+        {
+            path: '/home',
+            component: Home
         }
-    ]
+    ],
 })
---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="main">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Home",
+        props: {},
+        data() {
+            return {}
+        },
+        computed: {},
+        methods: {},
+        watch: {}
+    }
+</script>
+
+<style scoped>
+
+</style>
